400d320c2ab73e376ae37731e3f2e2500c91527a
subhakanta panda <meunknown466@gmail.com>
2022-12-14 23:54:07 +0530

    add

---
+++ JS/admin._panel.js
@@ -0,0 +1,14 @@
+console.log("app start")
+
+let add_btn=document.getElementById("new_product")
+add_btn.addEventListener("submit",function(event){
+    event.preventDefault()
+let add_img=document.querySelector("#img_url").value
+let add_name=document.querySelector("#name").value
+let add_price=document.querySelector("#price").value
+let add_des=document.querySelector("#des").value
+let obj={
+  
+}
+    console.log(add_img,add_name,add_price,add_des)
+})
